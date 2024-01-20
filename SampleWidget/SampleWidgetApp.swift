6cd4c02a36a25b46f71f925fdf04c07b580f14c5
kakizaki hayate <hayate.h.0704@gmail.com>
2024-01-21 00:35:37 +0900

    add: Initialize fileを追加

---
+++ SampleWidget/SampleWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleWidgetApp.swift
+//  SampleWidget
+//
+//  Created by 柿崎逸 on 2024/01/21.
+//
+
+import SwiftUI
+
+@main
+struct SampleWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
