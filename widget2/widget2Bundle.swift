e5938a572d8660db6a4b8ad59b1fe30d3d8dad59
kakizaki hayate <hayate.h.0704@gmail.com>
2024-01-21 18:07:27 +0900

    add: Widgetが実機ビルドできるようになった

---
+++ widget2/widget2Bundle.swift
@@ -0,0 +1,17 @@
+//
+//  widget2Bundle.swift
+//  widget2
+//
+//  Created by 柿崎逸 on 2024/01/21.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct widget2Bundle: WidgetBundle {
+    var body: some Widget {
+        widget2()
+        widget2LiveActivity()
+    }
+}
